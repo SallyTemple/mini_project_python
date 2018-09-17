2612bbf83804a9d9e0cf5d227fe2272f275459a6
SallyTemple <pfende@gmail.com>
2018-09-16 22:13:28 -0700

    update

--- find_cc_regex.py
+++ find_cc_regex.py
@@ -19,8 +19,10 @@ if __name__ == '__main__':
         4) Go through the file and print the total number of valid credit card numbers that you've found
     '''
     #### YOUR CODE HERE #####
-file = open ("ccnumbers.txt\n")
+
 file = "ccnumbers.txt"
 with open (file) as creditno:
     lines = creditno.readlines()
+
+
 
